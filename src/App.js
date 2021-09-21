2c7a5b0e9c5ab5c6eef868fa15a6ce49a125af98
Serzhana <buraevase@gmail.com>
2021-09-21 23:19:53 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React,{Component} from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
+class  Users extends Component {
+  constructor (props)
+  {super(props);
+  }
+
+  componentDidMount(){
+
+   axios.get("https://social-network.samuraijs.com/api/1.0/users")
+   .then(response => console.log("response", response.data))
+  }
+
+  render (){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<pИспользование библиотеки axios </p>
     </div>
   );
 }
+}
 
-export default App;
+
+
+export default Users;
